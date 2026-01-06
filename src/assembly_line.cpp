d52e1a615d398c5d3afde0512645048b550f1290
pratikchaudhari13052003 <pratikchaudhari13052003@gmail.com>
2026-01-06 20:35:38 +0530

    added assembly line

---
+++ src/assembly_line.cpp
@@ -0,0 +1,39 @@
+#include "bvm.h"
+
+int main(int argc, char *argv[]) {
+    
+    if (argc != 3) {
+        printf("Usage: %s input.asm output.bin\n", argv[0]);
+        return 1;
+    }
+
+    FILE *in = fopen(argv[1], "r");
+    FILE *out = fopen(argv[2], "wb");
+
+    if (!in || !out) {
+        perror("File error");
+        return 1;
+    }
+
+    uint8_t code[CODE_SIZE];
+    int pc = 0;
+    char line[LINE_SIZE];
+
+    while (fgets(line, sizeof(line), in)) {
+        if (line[0] == '\n' || line[0] == ';')
+            continue;
+        pc = assemble_line(line, code, pc);
+    }
+
+    fwrite(code, 1, pc, out);
+
+    fclose(in);
+    fclose(out);
+
+    printf("Assembled %d bytes into %s\n", pc, argv[2]);
+    return 0;
+
+}
+
+
+
