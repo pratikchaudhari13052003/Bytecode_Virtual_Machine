b9931721feb4436801f2dc3872e93e2f04fd270a
Abhinav Gupta <abhinavgupta@Abhinavs-MacBook-Air.local>
2026-01-08 13:24:16 +0530

    delete redundant tets

--- tests/arithmetic.asm
+++
@@ -1,8 +0,0 @@
-PUSH 10
-PUSH 20
-ADD         ; 10+20 = 30
-PUSH 5
-MUL         ; 30*5 = 150
-PUSH 10
-DIV         ; 150/10 = 15
-HALT
--- tests/functions.asm
+++
@@ -1,9 +0,0 @@
-PUSH 10
-CALL 14     ; PC 5: Call function at byte 14
-HALT        ; PC 10: Stop when function returns
-
-; Function: Double the value
-; START at Byte 14
-DUP         ; PC 14
-ADD         ; PC 15: Double it
-RET         ; PC 16: Return to Byte 10
